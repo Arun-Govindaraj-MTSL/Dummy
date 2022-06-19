d55937cf7bd5edf5942a4994fea22f50048952dc
Govindaraj <arun.govindaraj@mind.motherson.com>
2022-06-19 17:26:50 +0530

    Dummy

---
+++ Dummy/Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Dummy._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    
+    <asp:Button ID="showRecord" runat="server" Text="Show Record" OnClick="showRecord_Click"/>
+        <asp:GridView ID="gridRecord" runat="server"></asp:GridView>
+
+</asp:Content>
---
+++ Dummy/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Dummy
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void showRecord_Click(object sender, EventArgs e)
+        {
+            string ConString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(ConString);
+            string querystring = "Select * from Product";
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(querystring, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gridRecord.DataSource = dt;
+            gridRecord.DataBind();
+        }
+    }
+}
